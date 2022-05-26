29d3084950db08bcd3fb685afb172d1b14e78121
shri50 <shridharshende50@gmail.com>
2022-05-26 20:21:40 +0530

    added one api called prediction also added data.py or data cleaning

--- main.py
+++ main.py
@@ -7,5 +7,10 @@ def index():
     return "Default API"
 
 
+@app.route('prediction', methods =['GET','POST'])
+def prediction():
+    return "result"
+
+
 if __name__ == "__main__:
     app.run(debug=True)
